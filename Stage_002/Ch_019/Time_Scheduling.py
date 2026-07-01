d21d090a0bf5bf17285174ab88c9bfb7e8c64a71
Marc Moxey <moxeymarc@gmail.com>
2026-07-01 19:16:56 -0400

    edit

--- Stage_002/Ch_019/Time_Scheduling.py
+++ Stage_002/Ch_019/Time_Scheduling.py
@@ -42,3 +42,52 @@ print(new_years_2027 > halloween_2026)
 print(new_years_2027 != oct_31_2026)
 
 # Representing Durtion 
+delta = datetime.timedelta(days=11, hours=10, minutes=9, seconds=8)
+print(delta.days, delta.seconds, delta.microseconds)
+print(delta.total_seconds())
+print(str(delta))
+
+now = datetime.datetime.now()
+datetime.datetime(2026,12,2,18,38,50,636181)
+thousand_days = datetime.timedelta(days=1000)
+result = now + thousand_days
+print(result)
+
+oct_21st = datetime.datetime(2026, 10, 21,16,29,0)
+about_thirty_years = datetime.timedelta(days=365*30)
+print(oct_21st)
+print(oct_21st - about_thirty_years)
+print(oct_21st - (2* about_thirty_years))
+
+
+# Pausing Until a Specific date
+# halloween_2039 = datetime.datetime(2029, 10, 31, 0,0,0 )
+# while datetime.datetime.now() < halloween_2039:
+#     time.sleep(1) # wait 1 second before looping to check again
+    
+    
+
+# Converting datetime Objects into Strings 
+oct_21st = datetime.datetime(2026, 10, 21, 16, 29, 0)
+result = oct_21st.strftime('%Y/%m/%d %H:%M:%S')
+print(result)
+result = oct_21st.strftime('%I:%M %p')
+print(result)
+result = oct_21st.strftime("%B of %y")
+print(result)
+
+
+# Converting Strings into datetime Objects
+dt = datetime.datetime.strptime('October 21, 2026', '%B %d, %Y')
+print(dt)
+dt = datetime.datetime.strptime('2026/10/21 16:29:00', '%Y/%m/%d %H:%M:%S')
+print(dt)
+dt = datetime.datetime.strptime('October of 26', '%B of %y')
+print(dt)
+dt = datetime.datetime.strptime('November of 63', '%B of %y')
+print(dt)
+dt = datetime.datetime.strptime('November of 73', '%B of %y')
+print(dt)
+
+
+# Launching other programs from python
